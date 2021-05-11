f7b13d2d9807a540104080b7c007909103a07849
StefanS97 <stefan.stojkov97@gmail.com>
2021-05-11 22:01:21 +0200

    initial commit

---
+++ REST-API/app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+        return Post::create([
+            'title' => request('title'),
+            'content' => request('content'),
+        ]);
+    }
+
+    public function update(Post $post)
+    {
+        request()->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+        $success = $post->update([
+            'title' => request('title'),
+            'content' => request('content')
+        ]);
+        return ['success' => $success];
+    }
+
+    public function delete(Post $post)
+    {
+        $success = $post->delete();
+        return ['success' => $success];
+    }
+}
